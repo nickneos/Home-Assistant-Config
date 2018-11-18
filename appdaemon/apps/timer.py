98114a7a1e55cdf6d4e37fb3df066b95cf5f855d
so3n <nicholas.neos@gmail.com>
2018-11-18 23:33:48 +1100

    added zoned vacuum scripts; timers for AC

---
+++ appdaemon/apps/timer.py
@@ -0,0 +1,31 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+
+class Timer(hass.Hass):
+
+    def initialize(self):
+        self.handle = None
+        self.listen_state(self.timer, "input_select.timer_device", old = "Select...")
+    
+    def timer(self, entity, attribute, old, new, kwargs):
+        n = float(self.get_state("input_number.timer_hours"))
+
+        if new == "Main AC":
+            dev = "climate.living_room"
+        elif new == "Bedroom AC":
+            dev = "climate.bedroom"
+        else:
+            return
+        
+        if self.handle != None:
+            self.cancel_timer(self.handle)
+            self.log(f"Cancelling {self.handle}")
+
+        self.log(f"Turning off {dev} in {n} hour(s)")
+        self.handle = self.run_in(self.power_off, n*60*60, device = dev)
+        self.log(self.info_timer(self.handle))
+        self.set_state("input_select.timer_device", state = old)
+    
+    def power_off(self, kwargs):
+        self.turn_off(kwargs["device"])
+
