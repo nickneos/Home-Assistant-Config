ccb0fe641eff69dd31df916ae9e6861588321714
so3n <nicholas.neos@gmail.com>
2018-12-29 01:17:45 +1100

    xiaomi button app update

--- appdaemon/apps/xiaomi.py
+++ appdaemon/apps/xiaomi.py
@@ -6,66 +6,77 @@ class Button(hass.Hass):
 
     def initialize(self):
         for switch in self.args["bedroom_switch"]:
-            self.listen_event(self.bedroom_switch, "xiaomi_aqara.click", entity_id = switch)
+            self.listen_event(self.bedroom_switch, "xiaomi_aqara.click",
+                              entity_id = switch)
         
         for switch in self.args["outside_switch"]:
-            self.listen_event(self.outside_switch, "xiaomi_aqara.click", entity_id = switch)
+            self.listen_event(self.outside_switch, "xiaomi_aqara.click", 
+                              entity_id = switch)
         
         for switch in self.args["doorbell"]:
-            self.listen_event(self.doorbell, "xiaomi_aqara.click", entity_id = switch)
+            self.listen_event(self.doorbell, "xiaomi_aqara.click", 
+                              entity_id = switch)
 
     def bedroom_switch(self, event_name, data, kwargs):
-        click_type = data["click_type"]
-        click_dev = data["entity_id"]
+        device, entity = self.split_entity(data["entity_id"])
+        button = entity
+        click = data["click_type"]
         
-        if click_type == "single":
+        if click == "single":
             dev = "light.Bedroom"
-            self.log(f"{click_dev} pressed: click_type = {click_type} trigger = {dev}")
+            self.log(f"{button} pressed ({click})...action = {dev}")
             self.call_service("light/toggle", entity_id = dev)
           
-        elif click_type == "long_click_press":
+        elif click == "long_click_press":
             dev = "switch.master_switch"
-            self.log(f"{click_dev} pressed: click_type = {click_type} trigger = {dev}")
+            self.log(f"{button} pressed ({click})...action = {dev}")
             self.turn_off(dev)
         
-        elif click_type == "double":
+        elif click == "double":
             dev = "switch.template_soniq_tv"
-            self.log(f"{click_dev} pressed: click_type = {click_type} trigger = {dev}")
+            self.log(f"{button} pressed ({click})...action = {dev}")
             self.call_service("switch/toggle", entity_id = dev)
 
 
     def outside_switch(self, event_name, data, kwargs):
-        click_type = data["click_type"]
-        click_dev = data["entity_id"]
+        device, entity = self.split_entity(data["entity_id"])
+        button = entity
+        click = data["click_type"]
 
-        if click_type == "single":
-            dev = "switch.arlec_1b"
-            self.log(f"{click_dev} pressed: click_type = {click_type} trigger = {dev}")
+        if click == "single":
+            dev = "switch.flood_light"
+            self.log(f"{button} pressed ({click})...action = {dev}")
             self.call_service("switch/toggle", entity_id = dev)
 
-        elif click_type  == "double":
+        elif click  == "double":
             dev = "switch.arlec_1c"
-            self.log(f"{click_dev} pressed: click_type = {click_type} trigger = {dev}")
+            self.log(f"{button} pressed ({click})...action = {dev}")
             self.call_service("switch/toggle", entity_id = dev)
 
 
     def doorbell(self, event_name, data, kwargs):
         
-        # if data["click_type"] == "single":
-        #     script = "script/doorbell_1" if self.anyone_home() else "script/doorbell_2"
-        #     self.log(f"Doorbell pressed...executing {script}")
-        #     self.call_service(script)
-        
         if self.anyone_home():
-            self.call_service("xiaomi_aqara/play_ringtone", gw_mac = self.args["gw_mac"],
-                                ringtone_id = "10001", ringtone_vol = "25")
+            self.call_service("xiaomi_aqara/play_ringtone", 
+                               gw_mac = self.args["gw_mac"],
+                               ringtone_id = "10001", ringtone_vol = "25")
 
-        self.call_service("light/lifx_effect_pulse", entity_id = "group.all_lights", 
-                            brightness = "255", color_name = "green", period = "0.4", cycles = "10")
+        self.call_service("light/lifx_effect_pulse", 
+                           entity_id = "group.all_lights", 
+                           brightness = "255", color_name = "green", 
+                           period = "0.4", cycles = "10")
 
-        # if self.anyone_home():
-        #     self.call_service("media_player/play_media", entity_id = "media_player.google_home_group", 
-        #                         media_content_id = self.args["media_content_id"], media_content_type = "music")
+        if (self.anyone_home() and 
+            self.get_state("media_player.google_home_group") == "off" and
+            self.get_state("media_player.google_home_main") == "off" and
+            self.get_state("media_player.google_home_bedroom") == "off" and
+            self.get_state("media_player.google_home_office") == "off"):
+            # all above true
+            msg = ("Attention. There's someone at the door. "
+                   + "There's someone at the door") 
+            self.call_service("tts/google_say", 
+                              entity_id = "media_player.google_home_group", 
+                              message = msg)
 
         t = time.strftime("%d-%b-%Y %H:%M:%S")
         message = f"{t}: Doorbell pressed"
