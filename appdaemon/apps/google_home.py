dc0e3a50ae73c0986f3a223dc793e3479bb46788
so3n <nicholas.neos@gmail.com>
2019-08-13 17:30:00 +1000

    modify google home vol app

--- appdaemon/apps/google_home.py
+++ appdaemon/apps/google_home.py
@@ -2,18 +2,23 @@ import appdaemon.plugins.hass.hassapi as hass
 import datetime
 
 
-class Google_Home(hass.Hass):
+class Quiet_Mode(hass.Hass):
 
     def initialize(self):
-        self.run_daily(self.goolge_home_quiet_mode, datetime.time(23, 5), quiet = True)
-        self.run_daily(self.goolge_home_quiet_mode, datetime.time(6, 55), quiet = False)
-    
-    def goolge_home_quiet_mode(self, kwargs):
-        quiet_mode = kwargs["quiet"]
-        for gh in self.args["devices"]:
-            if quiet_mode:
-                vol = "0.2"
-            else:
-                vol = "0.6" if gh == "media_player.google_home_main" else "0.5"
-            self.log(f"Setting volume to {vol} for {gh}")
-            self.call_service("media_player/volume_set", entity_id = gh, volume_level = vol)
+        self.device = self.args["device"]
+        self.night_mode = self.args["night_mode"]
+        self.day_mode = self.args["day_mode"]
+
+        self.run_daily(self.gh_set_vol, 
+                        datetime.time(self.night_mode["hour"], self.night_mode["min"]), 
+                        vol = self.night_mode["vol"])
+        self.run_daily(self.gh_set_vol, 
+                        datetime.time(self.day_mode["hour"], self.day_mode["min"]), 
+                        vol = self.day_mode["vol"])
+
+    def gh_set_vol(self, kwargs):
+        gh = self.device
+        vol = kwargs["vol"]
+        
+        self.log(f"Setting volume to {vol} for {gh}")
+        self.call_service("media_player/volume_set", entity_id = gh, volume_level = vol)
