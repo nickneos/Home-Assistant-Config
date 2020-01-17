c5f17a0ef18196b9774863429a91429f96afdf48
so3n <nicholas.neos@gmail.com>
2020-01-17 22:49:10 +1100

    update reboot_router app

--- appdaemon/apps/other.py
+++ appdaemon/apps/other.py
@@ -5,17 +5,32 @@ class internet_down(hass.Hass):
 
     def initialize(self):
         self.utils = self.get_app("utils")
-        sensor = self.args["sensor"]
+        self.sensor = self.args["sensor"]
         duration = self.args["duration"]
         self.sec = self.utils.get_sec(duration)
 
-        self.listen_state(self.reboot_router_cb, sensor, new = "off", duration = self.sec)
+        self.listen_state(self.offline_cb, self.sensor, new = "off", duration = self.sec)
 
-    def reboot_router_cb(self, entity, attribute, old, new, kwargs):
-        self.log(f"Offline for {self.sec} seconds...Rebooting Router")
+    def offline_cb(self, entity, attribute, old, new, kwargs):
+        t = time.strftime("%d-%b-%Y %H:%M:%S")
+        self.log(f"Offline for {self.sec} seconds...Rebooting Router")        
+        self.call_service('persistent_notification/create',
+                title="Offline",
+                message=f"{t}: Offline for {self.sec} seconds...Rebooting Router"
+            )
+
+        self.reboot_router()
+
+    def reboot_router(self):
         self.call_service("shell_command/reboot_router")
-    
-    ### loop to check online
+        self.run_in(self.recheck_cb, 10*60)
+
+    def recheck_cb(self, kwargs):
+        if self.get_state(self.sensor) == "off":
+            self.log("Still offline...Rebooting Router")
+            self.reboot_router()
+        else:
+            self.log("Confirmed back online!")
 
 class milk_warmer(hass.Hass):
 
